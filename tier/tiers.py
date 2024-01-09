8481432b5e6f42eaef7c7801f30e2a0d5a358468
cfff226 <cazziednb@outlook.com>
2024-01-09 17:44:27 +0000

    Add a class which will be the parent class for the animals - Declare self, name, age and gender as parameters - Assign each of the parameters to the values of name, age, gender which have been passed into the class

--- tier/tiers.py
+++ tier/tiers.py
@@ -2,3 +2,9 @@
 
 # Tiernamen
 name = ["Mia", "Patch", "Hop", "Eric", "Winnie", "Lilly"]
+
+class Tier:
+    def __init__(self, name, alter, geschlecht):
+        self.name = name
+        self.alter = alter
+        self.geschlecht = geschlecht
