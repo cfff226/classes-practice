523fb87e93233d89c50a8102682bc2f05b598793
cfff226 <cazziednb@outlook.com>
2024-01-09 18:07:41 +0000

    Define functions which take in the parameter self, so that it takes in the variable that is passed into the class - Output the cat's name with it's favourite food - Output the cat's name with the sound that a cat typically makes

--- tier/tiers.py
+++ tier/tiers.py
@@ -34,3 +34,11 @@ class Katze(Tier):
         super().__init__(name, alter, geschlecht)
         self.klang = "meow"
         self.lieblingsessen = lieblingsessen
+
+    # Geben Sie den Namen und das Lieblingsfutter des Tieres aus
+    def printLieblingsessen(self):
+        print(f"\n{self.name}'s lieblingsessen ist {self.lieblingsessen}\n")
+
+    # Geben Sie den Lärm aus, den das Tier normalerweise macht
+    def printKlang(self):
+        print(f"\n{self.name} macht den Ton {self.klang}")
