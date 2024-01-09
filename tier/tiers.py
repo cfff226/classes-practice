5a668c65bb0caca7d29cb38e4d629336c6775688
cfff226 <cazziednb@outlook.com>
2024-01-09 17:51:29 +0000

    Add a class for the Rabbit - Pass Tier in as an argument for it to inhertit from the parent animal class - Apply super init to initialise the parameters passed into the animal class - Define a variable which will represent the sound that a rabbit typically makes - Define a variable which will take in a variable stating the rabbits favourite food

--- tier/tiers.py
+++ tier/tiers.py
@@ -3,8 +3,17 @@
 # Tiernamen
 name = ["Mia", "Patch", "Hop", "Eric", "Winnie", "Lilly"]
 
+# Elternklasse für die Tiere
 class Tier:
     def __init__(self, name, alter, geschlecht):
         self.name = name
         self.alter = alter
         self.geschlecht = geschlecht
+
+# Klasse für die Hase
+class Hase(Tier):
+    def __init__(self, name, alter, geschlecht, lieblingsessen):
+        super().__init__(name, alter, geschlecht)
+        self.klang = "squeek"
+        self.lieblingsessen = lieblingsessen
+
