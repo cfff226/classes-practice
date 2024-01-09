654c0d714f512889ff03a8ac458f4ce36714669b
cfff226 <cazziednb@outlook.com>
2024-01-09 18:04:07 +0000

    Define a class for the cat which will inherit from the main animal class - Pass in the arguments self, name, age, gender and favourite food - Declare a variable and assign it a string to represent the sound that a cat typically makes - Declare a variable and assign it the value of the cat's favourite food which will be passed into the class

--- tier/tiers.py
+++ tier/tiers.py
@@ -3,12 +3,14 @@
 # Tiernamen
 name = ["Mia", "Patch", "Hop", "Eric", "Winnie", "Lilly"]
 
+
 # Elternklasse für die Tiere
 class Tier:
     def __init__(self, name, alter, geschlecht):
         self.name = name
         self.alter = alter
         self.geschlecht = geschlecht
+
 
 # Klasse für die Hase
 class Hase(Tier):
@@ -17,10 +19,18 @@ class Hase(Tier):
         self.klang = "squeek"
         self.lieblingsessen = lieblingsessen
 
-# Geben Sie den Namen und das Lieblingsfutter des Tieres aus
+    # Geben Sie den Namen und das Lieblingsfutter des Tieres aus
     def printLieblingsessen(self):
         print(f"\n{self.name}'s lieblingsessen ist {self.lieblingsessen}\n\n")
 
-# Geben Sie den Lärm aus, den das Tier normalerweise macht
+    # Geben Sie den Lärm aus, den das Tier normalerweise macht
     def printKlang(self):
         print(f"{self.name} macht den Ton {self.klang}")
+
+
+# Klasse für die Katze
+class Katze(Tier):
+    def __init__(self, name, alter, geschlecht, lieblingsessen):
+        super().__init__(name, alter, geschlecht)
+        self.klang = "meow"
+        self.lieblingsessen = lieblingsessen
